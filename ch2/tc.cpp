0ef707d769757c2c570de77a8007d10bde5722a4
tech-note-hub <technotetn@gmail.com>
2026-01-05 22:53:14 +0630

    update ch2

---
+++ ch2/tc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int v1 = 3.99;
+    bool flag = -42;
+    unsigned char c = -2;
+    std::cout << v1 << std::endl;
+    std::cout << flag << std::endl;
+    std::cout << int(c) << std::endl;
+}
